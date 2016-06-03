e6ead3474f710d3ea81e2df6daeb3b4ae1984fc3
JTL <rakmial@gmail.com>
2016-06-03 09:52:37 -0700

    Starting files
    Python parser and csvs

---
+++ parse.py
@@ -0,0 +1,24 @@
+import os
+
+DATADIR = r"C:\Users\Bash\Desktop\Udacity\2_Data Analysis\P2"
+DATAFILE = "beatles-diskography.csv"
+
+def parse_file(datafile):
+    data = []
+    with open(datafile,'r') as f:
+        for line in f:
+            print line
+
+    return data
+
+def test():
+    # a simple test of your implemetation
+    datafile = os.path.join(DATADIR, DATAFILE)
+    d = parse_file(datafile)
+    firstline = {'Title': 'Please Please Me', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '22 March 1963', 'US Chart Position': '-', 'RIAA Certification': 'Platinum', 'BPI Certification': 'Gold'}
+    tenthline = {'Title': '', 'UK Chart Position': '1', 'Label': 'Parlophone(UK)', 'Released': '10 July 1964', 'US Chart Position': '-', 'RIAA Certification': '', 'BPI Certification': 'Gold'}
+
+    assert d[0] == firstline
+    assert d[9] == tenthline
+
+test()
